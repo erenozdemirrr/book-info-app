df0d5b5babc2b3b8b4304360d472d900bdabf13d
Eren Özdemir <eren.ozdemir@bestseller.com>
2022-07-06 14:24:46 +0300

    Add project files.

---
+++ KitapEvi_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using KitapEvi_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapEvi_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Kategoriler { get; set; }
+    }
+}
---
+++ KitapEvi_DataAccess/Migrations/20220705143954_deneme2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KitapEvi_DataAccess.Migrations
+{
+    public partial class deneme2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kategoriler",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategoriler", x => x.CategoryId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategoriler");
+        }
+    }
+}
